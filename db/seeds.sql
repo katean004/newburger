6aad50f864a1c6adf7b6195d47cdf4ed314add20
kate <kan004@ucr.edu>
2020-08-29 19:13:01 -0700

    updated seeds sql

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (name) VALUES ('Cheese');
+INSERT INTO burgers (name) VALUES ('Plain');
+INSERT INTO burgers (name, devour) VALUES ('Sloppy Joe', true);
+INSERT INTO burgers (name, devour) VALUES ('Double Double', true);
+INSERT INTO burgers (name, devour) VALUES ('Big Mac', true);
+INSERT INTO burgers (name) VALUES ('Veggie');
